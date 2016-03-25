1704044b3a5a807c9e4fd692e79be43456c5b632
s0913191 <s0913191@gmail.com>
2016-03-26 00:25:59 +0900

    aaa

---
+++ Surasura/Surasura1-2/CodeFile1.cs
@@ -0,0 +1,23 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+
+class Program
+{
+    public static void Main()
+    {
+        var button = new Button { Content = "Click" };
+        button.Click += (sender, e) => MessageBox.Show("みなさま、はじめまして");
+
+        var win = new Window
+        {
+            Width = 100,
+            Height = 50,
+            Content = button,
+
+        };
+
+        var app = new Application();
+        app.Run(win);
+    }
+}
---
+++ Surasura/Surasura1-3/CodeFile1.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Sample
+{
+    class Point2D
+    {
+        public int X { get; set; }
+        public int Y { get; set; }
+
+        public override string ToString()
+        {
+            return string.Format("({0},{1})", X, Y);
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var p = new Point2D { X = 1, Y = 2 };
+            Console.WriteLine(p);
+        }
+    }
+
+
+}
