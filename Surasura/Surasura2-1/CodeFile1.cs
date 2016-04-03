47bcf26bb89470a1f4d2413f53318d697556f97d
s0913191 <s0913191@gmail.com>
2016-04-03 20:03:00 +0900

    20160403

---
+++ Surasura/Surasura2-1/CodeFile1.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.Write("整数を入力してください");
+        var x = int.Parse(Console.ReadLine());
+        var y = x * x;
+        Console.WriteLine("{0} * {0} = {1}",x,y);
+
+    }
+
+
+}
+
---
+++ Surasura/Surasura2-2/CodeFile1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        int x = int.Parse(Console.ReadLine());
+        int y = (x > 5) ? 10 : 0;
+        int z = (y == 10) ? 1000 : 2;
+        Console.WriteLine("{0},{1}", y,z);
+    }
+}
